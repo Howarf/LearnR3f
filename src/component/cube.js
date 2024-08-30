72f2cf074f62cd539bc164346cd4a0e0481d1a4e
Howarf <dwarf0809@naver.com>
2024-08-30 11:43:15 +0900

    💬 add premier

--- src/component/cube.js
+++ src/component/cube.js
@@ -3,10 +3,11 @@ import { Canvas, useFrame } from '@react-three/fiber';
 import { useRef, useState } from 'react';
 
 function Box(props){
-    const ref = useRef();
+    const ref = useRef(); {/*랜더링에는 필요하지않지만 변화하는 값이 필요할때 쓰이는 Hook*/}
     const [hovered, hover] = useState(false);
     const [clicked, click] = useState(false);
-    useFrame((state, delta) =>{ (ref.current.rotation.x += delta); });
+    useFrame((state, delta) =>{ (ref.current.rotation.x += delta); }); {/*현재 프레임이 랜더링되기전 호출되는 Hook
+        state는 Three.js 객체의 상태이고 delta는 갱신속도를 의미 !!함수안에 setState는 절대 넣지 말것!!*/}
 
     return(
         <mesh 
@@ -16,8 +17,8 @@ function Box(props){
             onClick={(e)=>{click(!clicked)}}
             onPointerOver={(e)=>{e.stopPropagation(), hover(true)}}
             onPointerOut={(e)=>{hover(false)}}>
-            <boxGeometry args={[1, 1, 1]}/>
-            <meshStandardMaterial color={hovered ? 'hotpink':'orange'}/>
+            <boxGeometry args={[1, 1, 1]}/> {/*박스 형태를 만드는 태그*/}
+            <meshStandardMaterial color={hovered ? 'hotpink':'orange'}/> {/*기본 형태의 텍스쳐 태그 */}
         </mesh>
     )
 }
@@ -30,7 +31,7 @@ export default function Cube(){
             <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI}/>
             <Box position={[-1.2, 0, 0]}/>
             <Box position={[1.2, 0, 0]}/>
-            <OrbitControls/>
+            <OrbitControls/> {/*마우스로 화면을 움직이거나 확대 및 축소 등을 할 수 있게 해주는 태그*/}
         </Canvas>
     )
 }
